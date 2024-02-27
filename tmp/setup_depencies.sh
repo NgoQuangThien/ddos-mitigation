a4bef154d3912d4e91c791fc8d16dc23116b4b7c
NgoQuangThien <quangthien18032000@gmail.com>
2024-02-27 08:48:58 +0000

    Update: setup

--- tmp/setup_depencies.sh
+++ tmp/setup_depencies.sh
@@ -1,7 +1,11 @@
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 18
+rm -rf llvm.sh
 
 sudo ln -s /usr/bin/clang-18 /usr/local/bin/clang
 sudo ln -s /usr/bin/llvm-strip-18 /usr/local/bin/llvm-strip
 
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
